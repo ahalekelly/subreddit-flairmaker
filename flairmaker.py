f83f8884c9c60216919577cfae48533f5e5a9223
ahalekelly <ahalekelly@gmail.com>
2017-03-28 16:41:54 -0400

    Create flairmaker.py

---
+++ flairmaker.py
@@ -0,0 +1,25 @@
+oldName = "spritesheet.png"
+newName = "new_spritesheet.png"
+leftStripes = 4
+
+width = 80
+spriteHeight = 20
+
+from PIL import Image, ImageDraw
+import glob
+images = glob.glob("*.png")
+images.remove(oldName)
+images.remove(newName)
+print(images)
+oldSheet = Image.open(oldName)
+newSheet = Image.new('RGB', (width, oldSheet.height + len(images)*spriteHeight), color='white')
+newSheet.paste(oldSheet, (0,0))
+
+for i, imageName in enumerate(images):
+    newSheet.paste(Image.open(imageName), (leftStripes,oldSheet.height + i*spriteHeight))
+
+draw = ImageDraw.Draw(newSheet)
+for i in range(leftStripes+1):
+    draw.line([(i,0),(i,newSheet.height-1)], fill=oldSheet.getpixel((i,0)))
+    
+newSheet.save(newName)
